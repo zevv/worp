839755a8f468ea7733307e74b9fb466644208cd8
Ico Doornekamp <ico@pruts.nl>
2013-11-12 15:53:34 +0100

    Updated example

--- examples/gen.lua
+++ examples/gen.lua
@@ -3,33 +3,29 @@ j = Jack:new("worp")
 l = Linuxsampler:new("piano", "/opt/samples")
 m = Metro:new(150, 10)
 v = l:add("piano/megapiano.gig", 0)
+n = Dsp:Noise()
+nf = Dsp:Filter { type = "bp", f = 8000, Q = 5 }
+p = Dsp:Pan()
+a = Dsp:Adsr { A = 0, D = 0.03, S = 1, R = 0.05 }
+r = Dsp:Reverb { }
+f = Dsp:Filter { type = "bp", Q = 5 }
+lfo = Dsp:Osc { f = 4 / m:t_meas() }
 
 ns = { 36, 75, 79, 84, 34, 75, 79, 74, 84, 82 }
 
 function loop(i)
 	play(v, ns[i], ns[i] < 40 and 0.8 or 0.6, 1.0)
 	i = (i % #ns) + 1
-	at(m:t_beat(), one, i)
+	at(m:t_beat(), loop, i)
 end
-
-loop(1)
-
-f = Dsp:Filter { type = "bp", Q = 5 }
-lfo = Dsp:Osc { f = 4 / m:t_meas() }
 
 j:dsp("wah", 1, 1, function(vi)
 	f:set { f = lfo() * 500 + 700 }
 	return f(vi)
 end)
 
-n = Dsp:Noise()
-nf = Dsp:Filter { type = "bp", f = 8000, Q = 5 }
-p = Dsp:Pan()
-a = Dsp:Adsr { A = 0, D = 0.03, S = 1, R = 0.05 }
-r = Dsp:Reverb { }
-
 j:dsp("perc", 0, 2, function()
-	return p(r(nf(a() * n())))
+	return p( r( nf( a() * n() ) ) )
 end)
 
 function click()
@@ -40,6 +36,7 @@ function click()
 	at(m:t_beat() * 0.5, "click")
 end
 
+loop(1)
 click()
 
 j:connect("piano", "worp:wah")
